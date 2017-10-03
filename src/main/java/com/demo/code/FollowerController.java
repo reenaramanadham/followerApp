43c9248efee11b630622c4b5ab0992e12c3b2088
reenaramanadham <32320204+reenaramanadham@users.noreply.github.com>
2017-10-02 23:06:05 -0500

    Update FollowerController.java

--- src/main/java/com/demo/code/FollowerController.java
+++ src/main/java/com/demo/code/FollowerController.java
@@ -1,21 +1,15 @@
 package com.code.demo;
 
-/*import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-
-*/
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 
 @RestController
 public class FollowerController {
@@ -27,9 +21,6 @@ public class FollowerController {
     @Produces(MediaType.APPLICATION_JSON)
     public Followers userFollower(final @PathVariable("userId") String userId)
     {
-        //if(validate(userId))
         return followerService.fetchFollowers(userId) ;
-      
     }
-
 }
